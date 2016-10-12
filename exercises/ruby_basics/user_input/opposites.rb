bf2aac88aa99094ede223407297901fc80177f0e
Ricardo Rojas <Richi@Ricardos-MacBook-Pro.local>
2016-10-11 22:20:33 -0500

    add opposites attract exercises

---
+++ exercises/ruby_basics/user_input/opposites.rb
@@ -0,0 +1,35 @@
+# Write a program that requests two integers from the user, adds them together,
+# and then displays the result. Furthermore, insist that one of the integers be positive,
+# and one negative; however, the order in which the two integers are entered does not matter.
+
+# Do not check for positive/negative requirement until after both integers are entered,
+# and start over if the requirement is not met.
+
+# You may use the following method to validate input integers:
+
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
+
+def read_number
+  loop do
+    puts '>> Please enter a positive or negative integer:'
+    number = gets.chomp
+    return number.to_i if valid_number?(number)
+    puts '>> Invalid input. Only non-zero integers are allowed.'
+  end
+end
+
+first_number = nil
+second_number = nil
+
+loop do
+  first_number = read_number
+  second_number = read_number
+  break if first_number * second_number < 0
+  puts '>> Sorry. One integer must be positive, one must be negative.'
+  puts '>> Please start over.'
+end
+
+result = first_number.to_i + second_number.to_i
+puts ">> #{first_number} + #{second_number} = #{result}"
